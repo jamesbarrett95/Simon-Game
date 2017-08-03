9b8080ebf252395d74ec897e17810b0fd57b55fe
BBD\james.barrett <james.barrett@brightblueday.com>
2017-08-03 14:21:38 +0100

    Strict mode added, winner and loser notifications added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const strictButton = document.getElementById('strict')
 const counter = document.querySelector('.counter')
 const toggled = document.querySelector('.toggled')
 const pods = document.querySelectorAll('.pod')
-const winPromptContainer = document.querySelector('.win-prompt')
+const promptContainer = document.querySelector('.prompt')
 const playAgainButton = document.getElementById('play-again')
 
 let sequence = []
@@ -13,9 +13,11 @@ let playerIndex = 0
 let level = 1
 // A set flag that, once set to true, will halt the execution of getPattern()
 let cancelled = false
+let strictMode = false
 
 function enableStrict () {
   toggled.classList.toggle('enabled')
+  strictMode = !strictMode
 }
 
 function resetValues () {
@@ -24,20 +26,30 @@ function resetValues () {
   level = 1
 }
 
+function prompt (winner) {
+  const h2 = promptContainer.firstElementChild
+  promptContainer.style.display = 'block'
+  if (winner) {
+    h2.textContent = 'You win! Congratulations'
+  } else {
+    h2.textContent = 'Wrong selection, restarting game!'
+  }
+}
+
 function playAgain () {
-  winPromptContainer.style.display = 'none'
+  promptContainer.style.display = 'none'
   startButton.pointerEvents = 'auto'
   startButton.click()
 }
 
 function alterPods (action) {
   if (action) {
-    pods.forEach(function (pod) {
+    pods.forEach(pod => {
       pod.style.pointerEvents = 'auto'
       pod.style.cursor = 'pointer'
     })
   } else {
-    pods.forEach(function (pod) {
+    pods.forEach(pod => {
       pod.style.pointerEvents = 'none'
       pod.style.cursor = 'default'
     })
@@ -56,17 +68,23 @@ function checkUserSequence () {
     playerIndex++
     console.log('selection correct!')
   } else {
-    console.log('incorrect!')
+    if (strictMode) {
+      prompt(false)
+      return
+    }
+    counter.textContent = '--'
+    setTimeout(() => {
+      counter.textContent = level
+    }, 2000)
     alterPods(false)
     showPattern(sequence)
     playerIndex = 0
   }
 
   if (playerIndex === sequence.length) {
-    if (level === 2) {
+    if (level === 20) {
       alterPods(false)
-      console.log('you win!')
-      winPromptContainer.style.display = 'block'
+      prompt(true)
     } else {
       alterPods(false)
       console.log('level up!')
@@ -83,15 +101,27 @@ function checkUserSequence () {
 function showPattern (sequence) {
   let localSequence = [...sequence]
 
+  function speed () {
+    if (sequence.length < 5) {
+      return 2000
+    } else if (sequence.length >= 13) {
+      return 1000
+    } else if (sequence.length >= 9) {
+      return 1500
+    } else {
+      return 1750
+    }
+  }
+
   function addClassAfterTwoSeconds (el) {
-    if (cancelled) return
+    if (cancelled) return // Immediately return if the power has been turned off
     return new Promise(resolve => {
       setTimeout(_ => {
         const audio = el.firstElementChild
         el.classList.add('playing')
         audio.play()
         resolve()
-      }, 1500)
+      }, speed())
     })
   }
 
@@ -115,7 +145,7 @@ function getRandomPod () {
 function startGame () {
   resetValues()
   counter.textContent = level
-  this.style.pointerEvents = 'none'
+  startButton.style.pointerEvents = 'none'
   alterPods(false)
   getRandomPod()
 }
@@ -127,7 +157,7 @@ function power () {
     startButton.style.pointerEvents = 'auto'
     cancelled = false
   } else {
-    winPromptContainer.style.display = 'none'
+    promptContainer.style.display = 'none'
     options.classList.remove('visible')
     options.classList.add('hidden')
     startButton.style.pointerEvents = 'none'
